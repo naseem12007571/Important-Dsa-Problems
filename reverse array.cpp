e2b1d3b1f2c0f69b71338d7305cf219302c4fb45
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-09-25 12:00:29 +0530

    Create reverse array.cpp

---
+++ reverse array.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse (int arr[],int size){
+    int start=0;
+    int end=size-1;
+    while(start<=end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+    
+}
+
+
+void printarray(int arr[],int size){
+for(int i=0;i<size;i++){
+    cout<<arr[i]<<" ";
+}
+  cout<<endl;
+}
+
+
+int main(){
+    
+    int arr[6]={5,6,7,8,9,10};
+    int brr[5]= {1,2,3,4,5};
+    
+    reverse(arr,6);
+    reverse(brr,5);
+    
+    printarray(arr,6);
+    printarray(brr,5);
+    
+    
+}
