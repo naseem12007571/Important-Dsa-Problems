5dd3a6f946cc7c64ac810520e0ad5730cb5229f2
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-09-25 18:06:00 +0530

    coding ninja

--- Duplicate In Array.cpp
+++ Duplicate In Array.cpp
@@ -2,9 +2,11 @@ int findDuplicate(vector<int> &arr) {
   // Write your code here
 
   int ans=0;
+  //Xor all element
   for(int i=0;i<arr.size();i++){
       ans=ans^arr[i];
   }
+  // xor [1, n-1]
   for(int i=1;i<arr.size();i++){
       ans=ans^i;
   }
