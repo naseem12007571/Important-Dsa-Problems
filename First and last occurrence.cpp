ce1f1c919b2dbf56e9b9359247cd0e4f6eba754c
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-09-29 00:55:08 +0530

    Create First and last occurrence.cpp

---
+++ First and last occurrence.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+
+int firstOccurence(int arr[], int n, int key){
+    
+    int s=0;
+    int e=n-1;
+    
+    int  mid= s+(e-s)/2;
+    int ans= -1;
+    while(s<=e){
+        if(arr[mid]==key){
+            ans= mid;
+            e=mid-1;
+        } 
+        else if(key>arr[mid]){
+            s=mid+1;
+        }
+        else if(key<arr[mid]){
+            e=mid-1;
+        }
+        
+        mid= s+(e-s)/2;
+    }
+    return ans;
+    
+}
+int lastOccurence(int arr[], int n, int key){
+    
+    int s=0;
+    int e=n-1;
+    
+    int  mid= s+(e-s)/2;
+    int ans= -1;
+    while(s<=e){
+        if(arr[mid]==key){
+            ans= mid;
+            s=mid+1;
+        } 
+        else if(key>arr[mid]){    // go to right part
+            s=mid+1;
+        }
+        else if(key<arr[mid]){   // go to last left part
+            e=mid-1;
+        }
+        
+        mid= s+(e-s)/2;
+    }
+    return ans;
+    
+}
+
+int main()
+{
+    
+    int even[5]={1,2,3,3,5};
+    
+      cout<<"firstOccurence of 3 is "<<firstOccurence(even,5,3)<<endl;
+      cout<<"lastOccurence of 3 is "<<lastOccurence(even,5,3)<<endl;
+    return 0;
+}
