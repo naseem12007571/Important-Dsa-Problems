37538c69bb57a851d8259c8bdf88cf8fe12a6a52
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-10-01 16:23:13 +0530

    coding ninja and compilor

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -14,3 +14,32 @@ void selectionSort(vector<int>& arr, int n)
         swap(arr[miniIndex],arr[i]);
     }
 }
+
+
+//compilor code
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void search(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int miniIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[miniIndex])
+                miniIndex = j;
+        }
+        swap(arr[miniIndex], arr[i]);
+    }
+}
+
+int main() {
+    int arr[6] = {3, 4, 2, 1, 6, 7};
+    search(arr, 6);
+
+    cout << "Sorted array is ";
+    for (int i = 0; i < 6; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
