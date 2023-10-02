882b6f479ee5ac568b64d983942eb3f3a99d2a3b
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-10-02 11:04:34 +0530

    coding ninja

---
+++ bubble sort.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+    
+    cout<<" print the element 1 to 10";
+    for(int i=1;i<=n;i++){
+        cout<<" " << i;
+    }
+    
+}
