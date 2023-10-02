18a51777f5850dcf2e8b48ee4cb8c34a7de4b81e
Naseem Ahmad <naseemchoudhary18@gmail.com>
2023-10-02 11:20:45 +0530

    coding  ninja

--- bubble sort.cpp
+++ bubble sort.cpp
@@ -12,3 +12,30 @@ int main(){
     }
     
 }
+
+
+compilor code 
+#include<iostream>
+using namespace std;
+
+void bubble(int arr[], int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
+
+int main(){
+    
+    
+    int arr[6]={10,2,5,6,1,4};
+    bubble(arr,6);
+    for(int i=0;i<6;i++){
+        cout<<" "<<arr[i];
+    }
+     
+    
+}
